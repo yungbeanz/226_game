21dc26eb1d835a7f972b53c53e4009db96c0248f
yungbeanz <christiansgriffin@gmail.com>
2024-09-25 19:04:05 -0700

    Modified main and Board classes to suit the new player class, as well as make the overall program functional.

--- Board.py
+++ Board.py
@@ -1,4 +1,5 @@
 import random
+from Player import Player
 
 class Board:
 
@@ -27,7 +28,6 @@ class Board:
             self.board.append([])
             for j in range(0, self.boardSize):
                 self.board[i].append('_')
-        print(self.board)
 
         # Initializing array of treasures.
         for i in range(0, self.treasureSize):
@@ -35,7 +35,6 @@ class Board:
             for j in range(0, i+1):
                 self.treasures[i].append([])
                 self.treasures[i][j] = str(i+1)
-        print(self.treasures)
 
         # Randomly assign the treasure values to positions on the board.
         # First treasures are placed randomly, every following treasure checks if there is treasure in it's position to be placed.
@@ -43,7 +42,6 @@ class Board:
         while i < self.treasureSize:
             randomRow = random.randint(0, self.boardSize - 1)
             randomColumn = random.randint(0, self.boardSize - 1)
-            #print(str(randomRow) + ' | ' + str(randomColumn))
             # Check if positions are empty, upwards, right, left, then down. Then assign treasures this direction.
             if self.checkforopensurroundings(randomRow, randomColumn, i):
                 i += 1
@@ -54,8 +52,7 @@ class Board:
         # Call the function that creates the string version of the gameboard.
         self.__str__()
 
-    #def pick(self):
-
+    # Checks for open surroundings, returns false if there are no clear paths, returns true if there is.
     def checkforopensurroundings(self, row, column, i):
         openSpots = True
 
@@ -105,6 +102,14 @@ class Board:
 
         return openSpots
 
+    # Locates a position on the gameboard, sets the position to '_' and returns that value in that position
+    def pick(self, row, col):
+        treasureValue = self.board[row][col]
+        self.board[row][col] = '_'
+
+        return treasureValue
+
+    # Prints the gameboard
     def __str__(self):
         gameboardStr = ''
 
--- main.py
+++ main.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python3.11
 from Board import Board
+from Player import Player
 
+# Game setup
 b = Board(10, 4)
+username = input('Please enter your name: \n')
+p = Player(username)
+
+print('Welcome: ' + p.name)
 print(b)
+
+# Player guessing loop
+row = 0
+col = 0
+while row != '' and col != '':
+    try:
+        row = input('Please guess row of next treasure (0-9): \n')
+        col = input('Please guess column of next treasure (0-9): \n')
+        scoreFromGuess = b.pick(int(row), int(col))
+    except ValueError:
+        row=0
+        col=0
+        print('Please enter valid integer values')
+    except IndexError:
+        print('Please enter a number between 0-9')
+
+    if scoreFromGuess != '_':
+        p.addscore(int(scoreFromGuess))
+        print('Congratualations, you earned: ' + scoreFromGuess + ' points!')
+    else:
+        print('Close, but no cigar...')
+
+    print(p)
+    print(b)
+
