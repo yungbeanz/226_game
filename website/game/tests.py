815049f963c602af949a77c322dd9fa1e3f85945
yungbeanz <christiansgriffin@gmail.com>
2024-11-27 16:25:02 -0800

    Wrote Django Tests.

--- website/game/tests.py
+++ website/game/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+import sys
+sys.path.append("..")
+from Board import Board
+from Player import Player
 
 # Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from game.views import index, create, pick
+
+class GameViewsTest(TestCase):
+    def setUp(self):
+        # Call first url that displays game, and if players and board aren't created, creates two player objects and 100 grid board.
+        response = self.client.get(reverse('index'))
+
+        # Assert that the response status is 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+    def test_game_initialization(self):
+        # We import the globals to check their state
+        from game.views import playerOne, playerTwo, b
+
+        # Check that players are created correctly
+        self.assertIsInstance(playerOne, Player)
+        self.assertIsInstance(playerTwo, Player)
+        self.assertEqual(playerOne.name, "One")
+        self.assertEqual(playerTwo.name, "Two")
+
+        # Check that the board is initialized correctly
+        self.assertIsInstance(b, Board)
+        self.assertEqual(len(b.board), 100)
+
+    def test_score_update(self):
+        # Test for functionality where a player picks a tile and their score updates
+        # Example test to simulate score update functionality
+        response = self.client.get(reverse('pick', args=["One", 0, 0]))  # Pick the tile at (0, 0) for player "One"
+
+        # Check the score update based on the expected behavior
+        from game.views import playerOne, playerTwo
+        self.assertGreater(playerOne.score, 0)  # Check that playerOne's score increased after the pick
--- website/game/views.py
+++ website/game/views.py
@@ -5,9 +5,6 @@ sys.path.append("..")
 from Board import Board
 from Player import Player
 
-# Initializing variables, the create function must be called prior to any player choosing a location.
-# It seems that for some reason when the /create url browser is left open, when the /pick url is called,
-# it resets the board.
 playerOne = ''
 playerTwo = ''
 b = ''
@@ -15,10 +12,13 @@ b = ''
 def index(request):
         global playerOne
         global playerTwo
+        # Create players if it hasn't been done so already.
+        if playerOne == '' or playerTwo == '':
+                playerOne = Player("One")
+                playerTwo = Player("Two")
+
         global b
-
         return HttpResponse(f'<div style="display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center"><div style="background-color: orange; font-size: 35px; color: white; style: bold;">{playerOne} Score: {playerOne.score}</div><div style="font-size: 35px; background-color:red; color: white; style: bold;">{playerTwo} Score: {playerTwo.score}</div><div style="font-size: 25px; width: 10%; align-text: center;">{b}</div></div>')
-
 
 def greet(request, name):
         return HttpResponse(f'Hello {name}')
@@ -33,13 +33,19 @@ def create(request):
         b = Board(10, 4)
         return HttpResponse(f'<div style="display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center"><div style="background-color: orange; font-size: 35px; color: white; style: bold;">{playerOne} Score: {playerOne.score}</div><div style="font-size: 35px; background-color:red; color: white; style: bold;">{playerTwo} Score: {playerTwo.score}</div><div style="font-size: 25px; width: 10%; align-text: center;">{b}</div></div>')
 
-
 def pick(request, player, x, y):
         global playerOne
         global playerTwo
+        # Create players if it hasn't been done so already.
+        if playerOne == '' or playerTwo == '':
+                playerOne = Player("One")
+                playerTwo = Player("Two")
+
+        # Using previous classes logic that I had written.
         global b
         score = b.pick(int(x), int(y))
 
+        # Adding the score to the player objects.
         if player == "One":
                 playerOne.addscore(score)
         if player == "Two":
